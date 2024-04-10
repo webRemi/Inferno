6ce3c1ea98a8c1d29115d9443bdb0b39ffe5ca3b
asx <asx@asx.asx>
2024-04-10 20:57:50 +0200

    client styling

--- client.c
+++ client.c
@@ -12,21 +12,39 @@
 #define PORT 33333
 #define PORT_HTTP 80
 
+void box_error();
+
 //handle erros
 void error(char *message) {
-    fprintf(stderr, "%s: %s\n", message, strerror(errno));
+    char buf[1024];
+    sprintf(buf, "\033[31;5;208m[!] \033[0m");
+    fprintf(stderr, "%s%s: %s\n", buf, message, strerror(errno));
     exit(EXIT_FAILURE);
 }
 
+void banner();
+void info();
+void help_menu();
+void box_info();
+void box_error();
+void box_success();
 void client(int inferno_socket);
 void session(int inferno_socket);
 
+
+
 int main() {
+    banner();
+    info();
+
     //initialize socket
+    box_info();
     puts("Initializing socket...");
     int inferno_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (inferno_socket == -1)
         error("Initializing socket failed");
+    box_success();
+    puts("Socket initialized");
 
     // crafting all necessary for address
     struct sockaddr_in c2_address;
@@ -36,10 +54,12 @@ int main() {
     c2_address.sin_addr.s_addr = inet_addr(IP);
 
     //connect
-    puts("Connecting...");
+    box_info();
+    puts("Connecting to server...");
     if (connect(inferno_socket, (struct sockaddr *) &c2_address, sizeof(c2_address)) == -1)
-         error("Error connecting");
-    puts("Connected");
+         error("Error connecting to server");
+    box_success();
+    puts("Connected to server");
     client(inferno_socket);
 }
 
@@ -54,6 +74,19 @@ void client(int inferno_socket) {
          }
          request[strcspn(request, "\n")] = '\0';
 
+         if (strcmp(request, "help") == 0) {
+             help_menu();
+             client(inferno_socket);
+         }
+
+         if (strcmp(request, "exit") == 0) {
+             box_info();
+             puts("Exiting Inferno...");
+             box_success();
+             puts("Exited");
+             exit(EXIT_SUCCESS);
+         }
+
          //sending request
          if (send(inferno_socket, request, sizeof(request), 0) == -1)
              error("Error sending command to server");
@@ -62,13 +95,11 @@ void client(int inferno_socket) {
          char response[1024];
          if (recv(inferno_socket, response, sizeof(response), 0) == -1)
              error("Error receiving instruction from server");
+         box_success();
          printf("Result:\n%s\n", response);
 
-         if (strcmp(request, "http") == 0) {
-             printf("HTTP Listener started");
-             //session(inferno_socket);
-         }
          if (strcmp(response, "XXX") == 0) {
+             box_success();
              puts("Entering session");
              session(inferno_socket);
          }
@@ -87,6 +118,9 @@ void session(int inferno_socket) {
          }
          request[strcspn(request, "\n")] = '\0';
 
+         box_info();
+         printf("Sending %d bytes to server...\n", strlen(request));
+
          //sending request
          if (send(inferno_socket, request, sizeof(request), 0) == -1)
              error("Error sending command to server");
@@ -98,7 +132,63 @@ void session(int inferno_socket) {
          char response[1024];
          if (recv(inferno_socket, response, sizeof(response), 0) == -1)
              error("Error receiving instruction from server");
+
+         box_info();
+         printf("Receiving %d bytes from server...\n", strlen(response));
+         box_success();
          printf("Result:\n%s\n", response);
     }
     close(inferno_socket);
+}
+
+void banner() {
+    printf("\n");
+    printf("\033[38;5;208m");
+    printf("  _     <-. (`-')_            (`-')  _   (`-') <-. (`-')_                \n");
+    printf(" (_)       \\( OO) )  <-.      ( OO).-/<-.(OO )    \\( OO) )     .->       \n");
+    printf(" ,-(`-'),--./ ,--/(`-')-----.(,------.,------,),--./ ,--/ (`-')----.     \n");
+    printf(" | ( OO)|   \\ |  |(OO|(_\\---' |  .---'|   /`. '|   \\ |  | ( OO).-.  '    \n");
+    printf(" |  |  )|  . '|  |)/ |  '--. (|  '--. |  |_.' ||  . '|  |)( _) | |  |    \n");
+    printf("(|  |_/ |  |\\    | \\_)  .--'  |  .--' |  .   .'|  |\\    |  \\|  |)|  |    \n");
+    printf(" |  |'->|  | \\   |  `|  |_)   |  `---.|  |\\  \\ |  | \\   |   '  '-'  '    \n");
+    printf(" `--'   `--'  `--'   `--'     `------'`--' '--'`--'  `--'    `-----'     \n");
+    printf("\033[0m");
+}
+
+void info() {
+    printf("\033[1;31m");
+    char info[2048] = "X joined session";
+    printf("\nInfo: %s\n", info);
+    printf("\033[0m");
+}
+
+void help_menu() {
+    printf("\nCommand\t\tExample\t\t\tDescription\n");
+    printf("=======\t\t=======\t\t\t===========\n\n");
+    printf("help\t\t/\t\t\tShow this menu\n");
+    printf("exit\t\t/\t\t\tExit Inferno\n");
+    printf("listeners\t/\t\t\tlist active listeners\n");
+    printf("http\t\thttp 10.129.10.11 8080\tStart an HTTP listener\n");
+    printf("stop\t\tstop http 1\t\tStop an HTTP listener with ID X\n");
+    printf("sessions\t/\t\t\tList active sessions\n");
+    printf("enter\t\tenter 1\t\t\tEnter in session with ID X\n");
+    printf("kill\t\tkill 1\t\t\tDestroy session with ID X\n");
+}
+
+void box_info() {
+    printf("\033[38;5;208m");
+    printf("[>] ");
+    printf("\033[0m");
+}
+
+void box_error() {
+    printf("\033[31;5;208m");
+    printf("[!] ");
+    printf("\033[0m");
+}
+
+void box_success() {
+    printf("\033[32;5;208m");
+    printf("[+] ");
+    printf("\033[0m");
 }
